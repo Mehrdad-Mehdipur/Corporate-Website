0dadfd4b7bece9751573ade6639685346e29a221
Mwhrdad <mehrdad.mehdipur@gmail.com>
2023-03-03 13:02:28 +0330

    Navbar Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,7 @@
-// Scrolling 100vh
+// ===== Show Search bar =====
+const showSearchBar = document.querySelector("#showSearchBar");
+const searchBar = document.querySelector("#searchBar");
 
-/*
-const e1 = document.querySelector("#e1");
-console.log(e1);
-
-let pageHeight = window.innerHeight;
-
-document.onscroll = () => {
-  document.body.scrollTop = 0;
+showSearchBar.onclick = () => {
+  searchBar.classList.toggle("show");
 };
-
-document.onwheel = (e) => {
-  // console.log(e.deltaY);
-  if (e.deltaY > 0) {
-    scrollDown();
-  } else {
-    scrollUp();
-  }
-};
-
-scrollDown = () => {
-  document.body.style.transform =
-    "translate3d(0px, -" + pageHeight + "px, 0px)";
-};
-
-scrollUp = () => {
-  document.body.style.transform = "translate3d(0px, 0px, 0px)";
-};
-*/
-
-// ====================================
