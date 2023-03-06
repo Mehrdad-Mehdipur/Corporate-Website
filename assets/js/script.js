a959e3c65cbcd4cb71924443c60764ccb32572c3
Mwhrdad <mehrdad.mehdipur@gmail.com>
2023-03-07 00:47:38 +0330

    some changes in navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
-// ===== Show Search bar =====
+//* Navbar
+
+// ========== Show Search bar ==========
 const showSearchBar = document.querySelector("#showSearchBar");
 const searchBar = document.querySelector("#searchBar");
 
 showSearchBar.onclick = () => {
   searchBar.classList.toggle("show");
 };
+
+// ========== Change Image By Hover On The Links ==========
+
+const navProjectsLinks = document.querySelector("#navProjectsLinks").children;
+const navProjectsImg = document.querySelector("#navProjectsImg").children;
+
+for (let i = 0; i < navProjectsLinks.length; i++) {
+  navProjectsLinks[i].addEventListener("mouseover", function () {
+    const id = this.getAttribute("data-id");
+    for (let r = 0; r < navProjectsImg.length; r++) {
+      navProjectsImg[r].classList.remove("active");
+    }
+    navProjectsImg[id].classList.add("active");
+  });
+}
