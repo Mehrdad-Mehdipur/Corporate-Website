2084e5c329044bb031b5aa276743ab602e94a2c6
SamiraPorkhaligh <samira.pourkhaligh@gmail.com>
2023-03-23 02:01:14 +0430

    Display slide image

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,1 +1,16 @@
 // //* Portfolio
+
+const images = document.querySelectorAll(".slide img");
+let display = document.querySelector(".mainPic");
+
+images.forEach(image => {
+  image.addEventListener("click", () => {
+    const id = image.getAttribute("data-id");
+    console.log(id);
+    const newImage = document.createElement("img");
+    newImage.src = `./assets/images/pic${id}.jpg`;
+    display.innerHTML = "";
+    display.appendChild(newImage);
+  })
+})
+
