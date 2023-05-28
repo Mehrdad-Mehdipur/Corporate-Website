57438429ae4ba29d32c72dee04d3cc08fa705bc6
SamiraPorkhaligh <samira.pourkhaligh@gmail.com>
2023-05-28 14:34:54 +0330

    Write style for Blog section

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,7 @@
 
 let titles = document.querySelectorAll("#blog .description h3");
 const descriptions = document.querySelectorAll("#blog .description p");
+const newBlog = document.querySelector("#blog .newBlog .description p");
 
 // Title length less than 60 characters
 titles.forEach((title) => {
@@ -17,5 +18,15 @@ descriptions.forEach((description) => {
 		if (description.innerHTML.length > 120) {
 			description.innerHTML = description.innerHTML.substring(0, 120) + "...";
 		}
+		if (window.innerWidth <= 900) {
+			if (description.innerHTML.length > 100) {
+				description.innerHTML = description.innerHTML.substring(0, 100) + "...";
+			}
+		}
+		// if (window.innerWidth <= 768) {
+		// 	if (description.innerHTML.length > 70) {
+		// 		description.innerHTML = description.innerHTML.substring(0, 70) + "...";
+		// 	}
+		// }
 	});
 })
