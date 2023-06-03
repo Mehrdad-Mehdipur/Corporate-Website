cbaea2cceec79e6c8225ae3921ac9649a095b725
SamiraPorkhaligh <samira.pourkhaligh@gmail.com>
2023-06-03 15:35:48 +0330

    Edit  js for Blog section

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,32 +1,42 @@
-
 let titles = document.querySelectorAll("#blog .description h3");
 const descriptions = document.querySelectorAll("#blog .description p");
 const newBlog = document.querySelector("#blog .newBlog .description p");
 
-// Title length less than 60 characters
+// Title length less than 85 characters
 titles.forEach((title) => {
-	document.addEventListener("DOMContentLoaded", function () {
-		if (title.innerHTML.length > 60) {
-			title.innerHTML = title.innerHTML.substring(0, 60);
-		}
-	});
-})
+  document.addEventListener("DOMContentLoaded", function () {
+    if (title.innerHTML.length > 85) {
+      title.innerHTML = title.innerHTML.substring(0, 85);
+    }
+  });
+});
 
 // Descriptions length less than 60 characters
-descriptions.forEach((description) => {
-	document.addEventListener("DOMContentLoaded", function () {
-		if (description.innerHTML.length > 120) {
-			description.innerHTML = description.innerHTML.substring(0, 120) + "...";
-		}
-		if (window.innerWidth <= 900) {
-			if (description.innerHTML.length > 100) {
-				description.innerHTML = description.innerHTML.substring(0, 100) + "...";
-			}
-		}
-		// if (window.innerWidth <= 768) {
-		// 	if (description.innerHTML.length > 70) {
-		// 		description.innerHTML = description.innerHTML.substring(0, 70) + "...";
-		// 	}
-		// }
-	});
-})
+let originalTexts = [];
+
+function handleOriginalText() {
+  descriptions.forEach((description) => {
+    originalTexts.push(description.innerHTML);
+  });
+}
+
+function handleDescriptions() {
+  descriptions.forEach((description, index) => {
+    if (window.innerWidth <= 900) {
+      description.innerHTML = originalTexts[index].slice(0, 100) + "...";
+    } else {
+      description.innerHTML = originalTexts[index];
+      description.innerHTML = description.innerHTML.slice(0, 140) + "...";
+    }
+
+    if (index === 2 && window.innerWidth > 768) {
+      description.innerHTML = originalTexts[index].slice(0, 300) + "...";
+    }
+  });
+}
+
+window.addEventListener("resize", handleDescriptions);
+document.addEventListener("DOMContentLoaded", () => {
+  handleOriginalText();
+  handleDescriptions();
+});
